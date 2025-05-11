19e4226d5e87b7b6723bea96fd7fae921c5b0771
eDanSaMa <danielsalasmtz11@gmail.com>
2025-05-10 18:21:55 -0600

    Servicio by Dan

--- src/app/components/servicio/servicio.component.ts
+++ src/app/components/servicio/servicio.component.ts
@@ -4,19 +4,29 @@ import { ServicioService } from '../../services/servicio.service';
 import { RouterModule, ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { FormsModule } from '@angular/forms';
-import { NgForm } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-servicio',
-  imports: [RouterModule, CommonModule, MatProgressSpinnerModule, FormsModule, MatFormFieldModule, MatInputModule, MatSnackBarModule, MatIconModule, MatCheckboxModule, MatButtonModule],
+  imports: [RouterModule, 
+    CommonModule, 
+    MatProgressSpinnerModule, 
+    FormsModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatSnackBarModule, 
+    MatIconModule, 
+    MatCheckboxModule, 
+    MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatSelectModule],
   templateUrl: './servicio.component.html',
   styleUrl: './servicio.component.css'
 })
@@ -25,24 +35,39 @@ export class ServicioComponent {
   id!: number;
   modelo = {
     nombre: '',
+    nombrePublico: '',
     email: '',
     duda: '',
-    aceptaFAQ: false
+    aceptaFAQ: false,
+    fechaParticular: null
   };
+  minFecha = new Date();
 
   @ViewChild('formulario') formularioElement!: ElementRef;
 
   serImage: {[key: number]: string[]} = {
-    1: ["images/s1-1.jpg","images/s1-2.jpg","images/s1-3.jpg"],
-    2: ["images/s2-1.jpg","images/s2-2.jpg","images/s2-3.jpg"],
+    1: ["images/s1-1.jpg","images/s1-2.webp","images/s1-3.jpg"],
+    2: ["images/s2-1.jpg","images/s2-2.webp","images/s2-3.jpg"],
     3: ["images/s3-1.jpg","images/s3-2.jpg","images/s3-3.jpg"],
-    4: ["images/s4-1.jpg","images/s4-2.jpg","images/s4-3.jpg"],
-    5: ["images/s5-1.jpg","images/s5-2.jpg","images/s5-3.jpg"],
-    6: ["images/s6-1.jpg","images/s6-2.jpg","images/s6-3.jpg"],
-    7: ["images/s7-1.jpg","images/s7-2.jpg","images/s7-3.jpg"],
+    4: ["images/s4-1.jpg","images/s4-2.jfif","images/s4-3.jpg"],
+    5: ["images/s5-1.jpg","images/s5-2.webp","images/s5-3.jpg"],
+    6: ["images/s6-1.jpg","images/s6-2.jfif","images/s6-3.webp"],
+    7: ["images/s7-1.jpg","images/s7-2.jpg","images/s7-3.avif"],
     8: ["images/s8-1.jpg","images/s8-2.jpg","images/s8-3.jpg"],
-    9: ["images/s9-1.jpg","images/s9-2.jpg","images/s9-3.jpg"],
+    9: ["images/s9-1.png","images/s9-2.webp","images/s9-3.jfif"],
   };
+
+  serName: string[] = [
+    "SPA",
+    "Restaurant",
+    "Gimnasio",
+    "Albercas",
+    "Bar",
+    "Salón de eventos",
+    "Sala de Reuniones",
+    "Limpieza y Lavandería",
+    "Transportes"
+  ]
 
   constructor(public service: ServicioService, public route: ActivatedRoute, private snackBar: MatSnackBar) {
 
--- src/app/servicio.ts
+++ src/app/servicio.ts
@@ -9,6 +9,6 @@ export const SERVICIOS: Servicios[] = [
     { id: 5, title: "Bar", info: "Nunca puede faltar una buena bebida", image: 'images/s5-1.jpg' },
     { id: 6, title: "Salón de Eventos", info: "Salones para tus eventos especiales", image: 'images/s6-1.jpg' },
     { id: 7, title: "Sala de Reuniones", info: "Para tus proyectos mientras te relajas", image: 'images/s7-1.jpg' },
-    { id: 8, title: "Lavandería", info: "Nosotros lavamos tus prendas, tu solo relajate", image: 'images/s8-1.jpg' },
-    { id: 9, title: "Transporte", info: "Nosotros te llevamos por el agua", image: 'images/s9-1.jpg' }
+    { id: 8, title: "Limpieza y Lavandería", info: "Nosotros limpiamos el lugar y lavamos tu ropa, solo disfruta", image: 'images/s8-1.jpg' },
+    { id: 9, title: "Transportes", info: "Nosotros te llevamos por mar y por tierra", image: 'images/s9-1.png' }
 ];
