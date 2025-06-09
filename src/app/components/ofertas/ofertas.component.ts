e54ae279d3debf8dfc3bfb6736e02e90e4eeff23
Wawa4442 <easybrandylan123@gmail.com>
2025-06-08 18:06:01 -0600

    Se añadieron las siguientes secciones, una seccion de preguntas y respuestas para las personas que visiten la pagina, una seccion de cupones en las ofertas y se añadio el diseño responsive para la pagina del inicio

--- src/app/components/ofertas/ofertas.component.ts
+++ src/app/components/ofertas/ofertas.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-ofertas',
@@ -10,5 +11,24 @@ import { RouterModule } from '@angular/router';
 export class OfertasComponent {
   ngOnInit(){
     window.scrollTo({ top: 0 });
+  }
+
+   cupones = [
+    { id: 'cupon1', titulo: '10% en Spa', codigo: 'SPA10' },
+    { id: 'cupon2', titulo: '2x1 en bebidas', codigo: 'BEBIDA2X1' },
+    { id: 'cupon3', titulo: '25% en tours', codigo: 'TOUR25' },
+    { id: 'cupon4', titulo: '20% en Zona Kids', codigo: 'KIDS20' },
+    { id: 'cupon5', titulo: 'Cena romántica gratis', codigo: 'LOVE100' }
+  ];
+
+  copiarAlPortapapeles(id: string) {
+    const elemento = document.getElementById(id);
+    if (elemento) {
+      navigator.clipboard.writeText(elemento.innerText).then(() => {
+        Swal.fire('¡Cupon Copiado!','Disfruta de tu beneficio', 'success');
+      }, () => {
+        Swal.fire('El cupon no se pudo copiar','No sabemos lo que sucedio', 'error');
+      });
+    }
   }
 }
