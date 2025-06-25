3b45d0128ee572e88829546af94c70e5d94cb81e
eDanSaMa <danielsalasmtz11@gmail.com>
2025-06-25 15:02:28 -0600

    Correccion

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
+}).catch((err) => console.error(err));
